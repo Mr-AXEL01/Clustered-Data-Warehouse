dfdc5926247071400cf359301f944a5da13b0f5a
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2025-06-18 00:08:41 +0100

    FEAT : Register CurrencyProvider and inject into FxDealValidationService via config
    this allows keeping the core service logic independent of the framework.

--- src/main/java/com/progressoft/clustereddatawarehouse/config/FxDealConfig.java
+++ src/main/java/com/progressoft/clustereddatawarehouse/config/FxDealConfig.java
@@ -3,8 +3,10 @@ package com.progressoft.clustereddatawarehouse.config;
 import com.progressoft.clustereddatawarehouse.mapper.FxDealMapper;
 import com.progressoft.clustereddatawarehouse.mapper.FxDealMapperImpl;
 import com.progressoft.clustereddatawarehouse.repository.FxDealRepository;
+import com.progressoft.clustereddatawarehouse.service.CurrencyProvider;
 import com.progressoft.clustereddatawarehouse.service.FxDealService;
 import com.progressoft.clustereddatawarehouse.service.FxDealValidationService;
+import com.progressoft.clustereddatawarehouse.service.impl.SimpleCurrencyProvider;
 import com.progressoft.clustereddatawarehouse.service.impl.FxDealServiceImpl;
 import com.progressoft.clustereddatawarehouse.service.impl.FxDealValidationServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -19,16 +21,21 @@ public class FxDealConfig {
     }
 
     @Bean
-    FxDealValidationService fxDealValidationService() {
-        return new FxDealValidationServiceImpl();
+    FxDealValidationService fxDealValidationService(final CurrencyProvider currencyProvider) {
+        return new FxDealValidationServiceImpl(currencyProvider);
     }
 
     @Bean
     FxDealService fxDealService(
-            FxDealRepository repository,
-            FxDealValidationService validator,
-            FxDealMapper mapper
+            final FxDealRepository repository,
+            final FxDealValidationService validator,
+            final FxDealMapper mapper
     ) {
         return new FxDealServiceImpl(repository, validator, mapper);
+    }
+
+    @Bean
+    CurrencyProvider currencyProvider() {
+        return new SimpleCurrencyProvider();
     }
 }
