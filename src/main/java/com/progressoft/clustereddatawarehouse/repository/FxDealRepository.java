168c35225569cc9c34c2b4aae54abd90166c30c7
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2025-06-17 04:43:29 +0100

    FEAT : Add FxDealRepository for data access
    define repository interface and extends from jpaRepository

---
+++ src/main/java/com/progressoft/clustereddatawarehouse/repository/FxDealRepository.java
@@ -0,0 +1,7 @@
+package com.progressoft.clustereddatawarehouse.repository;
+
+import com.progressoft.clustereddatawarehouse.domain.FxDeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FxDealRepository extends JpaRepository<FxDeal, String> {
+}
